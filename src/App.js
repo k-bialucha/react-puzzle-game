ef3efb5b7c4c6d4ffe470dbc7e10248639410717
Kamil Bialucha <kamil.bialucha@gmail.com>
2018-07-26 18:14:10 +0200

    DropReceiver and DraggableElement utilized in the App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import DropReceiver from './DropReceiver/DropReceiver';
+import DraggableElement from './DraggableElement/DraggableElement';
+
 import './App.css';
 
 class App extends Component {
@@ -7,12 +10,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <DraggableElement />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <DropReceiver />
       </div>
     );
   }
