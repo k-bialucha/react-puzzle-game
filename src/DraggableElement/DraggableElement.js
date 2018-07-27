31b3c6ba927458c367370d69612274acdaa27184
Kamil Bialucha <kamil.bialucha@gmail.com>
2018-07-27 08:33:58 +0200

    OnDaragStart event handled in the DraggableElement

--- src/DraggableElement/DraggableElement.js
+++ src/DraggableElement/DraggableElement.js
@@ -3,13 +3,17 @@ import React from 'react';
 import logo from '../logo.svg';
 
 class DraggableElement extends React.PureComponent {
+    handleDrag(event, type) {
+        event.dataTransfer.effectAllowed = "move";
+        event.dataTransfer.setData('dataType', type)
+    }
     render() {
         return (
             <img
                 src={logo}
                 className="App-logo" 
                 draggable 
-                onDragStart={event => console.log(event)}
+                onDragStart={event => this.handleDrag(event, 'image')}
             />
         );
     }
