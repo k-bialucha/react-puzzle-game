584b282b34cab27c63eb940b56c2b1a7d917c1f2
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-07-28 11:16:31 +0200

    DroppableElement ID passed to the drag event

--- src/DraggableElement/DraggableElement.js
+++ src/DraggableElement/DraggableElement.js
@@ -3,9 +3,10 @@ import React from 'react';
 import logo from '../logo.svg';
 
 class DraggableElement extends React.PureComponent {
-    handleDrag(event, type) {
+    handleDrag(event, type, id) {
         event.dataTransfer.effectAllowed = "move";
         event.dataTransfer.setData('dataType', type)
+        event.dataTransfer.setData('id', id)
     }
     render() {
         return (
@@ -13,7 +14,7 @@ class DraggableElement extends React.PureComponent {
                 src={logo}
                 className="App-logo" 
                 draggable 
-                onDragStart={event => this.handleDrag(event, 'image')}
+                onDragStart={event => this.handleDrag(event, 'image', this.props.id)}
             />
         );
     }
