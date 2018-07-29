e3a710837dd0150714f7d3bc9df1de5308f97e7b
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-07-29 20:29:31 +0300

    DropReceiver elements created from a parameter

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -4,19 +4,22 @@ import DropReceiver from '../DropReceiver/DropReceiver';
 
 import './Grid.css';
 
+const GRID_SIZE = 3;
+
+const gridElements = [];
+
+for (let rowIndex = 1; rowIndex <= GRID_SIZE; rowIndex++) {
+    for (let colIndex = 1; colIndex <= GRID_SIZE; colIndex++) {
+        const correctAnswerId = `${rowIndex}${colIndex}`
+        gridElements.push(<DropReceiver key={correctAnswerId} correctAnswerId={correctAnswerId} />)
+    }
+}
+
 class Grid extends React.PureComponent {
     render() {
         return (
             <div className="Grid">
-                <DropReceiver />
-                <DropReceiver />
-                <DropReceiver />
-                <DropReceiver />
-                <DropReceiver />
-                <DropReceiver />
-                <DropReceiver />
-                <DropReceiver />
-                <DropReceiver />
+                {gridElements}
             </div>
         );
     }
