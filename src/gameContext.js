78e4a4cb5d61fa2d2d1a7ff29e5c7453cf9b446e
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-07-30 00:01:37 +0300

    Puzzle IDs in the gameContext changed to be strings

--- src/gameContext.js
+++ src/gameContext.js
@@ -11,15 +11,15 @@ import row3col2 from "./assets/images/row-3-col-2.png";
 import row3col3 from "./assets/images/row-3-col-3.png";
 
 const puzzles = [
-    { id: 11, image: row1col1 },
-    { id: 12, image: row1col2 },
-    { id: 13, image: row1col3 },
-    { id: 21, image: row2col1 },
-    { id: 22, image: row2col2 },
-    { id: 23, image: row2col3 },
-    { id: 31, image: row3col1 },
-    { id: 32, image: row3col2 },
-    { id: 33, image: row3col3 }
+    { id: "11", image: row1col1 },
+    { id: "12", image: row1col2 },
+    { id: "13", image: row1col3 },
+    { id: "21", image: row2col1 },
+    { id: "22", image: row2col2 },
+    { id: "23", image: row2col3 },
+    { id: "31", image: row3col1 },
+    { id: "32", image: row3col2 },
+    { id: "33", image: row3col3 }
 ];
 
 const Context = React.createContext();
