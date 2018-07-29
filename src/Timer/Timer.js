a821573023099c7bc86c418d11581e49597b9ab2
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-07-29 22:44:38 +0300

    Timer component message changed

--- src/Timer/Timer.js
+++ src/Timer/Timer.js
@@ -5,12 +5,7 @@ import { withGameContext } from '../gameContext';
 const Timer = props =>
     <div>
         {props.game.timer === 0 ?
-            <React.Fragment>
-                <h3>Click to start the game</h3>
-                <button onClick={props.game.startGame}>
-                    Start Game
-                </button>
-            </React.Fragment>
+            <h3>Drag the puzzle to start the game</h3>
             : <h3>Time: {props.game.timer}</h3> 
         }
     </div>;
