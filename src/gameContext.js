4758d28dcdce9be7a2b7db7b3282920095afaa32
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-07-29 21:58:55 +0300

    Puzzle elements added to the state of the GameProvider component

--- src/gameContext.js
+++ src/gameContext.js
@@ -1,12 +1,35 @@
 import React from 'react';
 
+import row1col1 from "./assets/images/row-1-col-1.png";
+import row1col2 from "./assets/images/row-1-col-2.png";
+import row1col3 from "./assets/images/row-1-col-3.png";
+import row2col1 from "./assets/images/row-2-col-1.png";
+import row2col2 from "./assets/images/row-2-col-2.png";
+import row2col3 from "./assets/images/row-2-col-3.png";
+import row3col1 from "./assets/images/row-3-col-1.png";
+import row3col2 from "./assets/images/row-3-col-2.png";
+import row3col3 from "./assets/images/row-3-col-3.png";
+
+const puzzles = [
+    { id: 11, image: row1col1 },
+    { id: 12, image: row1col2 },
+    { id: 13, image: row1col3 },
+    { id: 21, image: row2col1 },
+    { id: 22, image: row2col2 },
+    { id: 23, image: row2col3 },
+    { id: 31, image: row3col1 },
+    { id: 32, image: row3col2 },
+    { id: 33, image: row3col3 }
+];
+
 const Context = React.createContext();
 
 class GameProvider extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-            timer: 0
+            timer: 0,
+            puzzles
         }
     }
     startGame() {
@@ -28,7 +51,8 @@ class GameProvider extends React.PureComponent {
             <Context.Provider
                 value={{
                     timer: this.state.timer,
-                    startGame: this.startGame.bind(this)
+                    puzzles: this.state.puzzles,
+                    startGame: this.startGame.bind(this),
                 }}
             >
                 {this.props.children}
