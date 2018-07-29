be54c87745dd2f2f2cff4cd0829e115ce3c23de5
Kamil Bialucha <210347@student.pwr.edu.pl>
2018-07-29 07:42:08 +0200

    IsDragged state property implemented in the DraggableElement state; DraggableElement-image--dragged style updated

--- src/DraggableElement/DraggableElement.js
+++ src/DraggableElement/DraggableElement.js
@@ -5,18 +5,33 @@ import './DraggableElement.css';
 import logo from '../logo.svg';
 
 class DraggableElement extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isDragged: false
+        };
+    }
     handleDrag(event, type, id) {
+        this.setState({
+            isDragged: true
+        });
         event.dataTransfer.effectAllowed = "move";
         event.dataTransfer.setData('dataType', type)
         event.dataTransfer.setData('id', id)
+    }
+    handleDragEnd() {
+        this.setState({
+            isDragged: false
+        });
     }
     render() {
         return (
             <img
                 src={this.props.image || logo}
-                className={"DraggableElement-image" + this.state.isDragged ? " DraggableElement-image--dragged" : ""}
+                className={"DraggableElement-image" + (this.state.isDragged ? " DraggableElement-image--dragged" : "")}
                 draggable 
                 onDragStart={event => this.handleDrag(event, 'image', this.props.id)}
+                onDragEnd={this.handleDragEnd.bind(this)}
             />
         );
     }
